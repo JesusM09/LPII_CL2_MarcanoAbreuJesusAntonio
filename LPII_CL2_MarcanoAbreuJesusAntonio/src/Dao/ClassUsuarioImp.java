5816250430c03aa8658033121735c1ee84b69e92
JesusM09 <marcano.jesus04@gmail.com>
2024-06-23 10:06:14 -0500

    adding some test and jpa comands and views

---
+++ LPII_CL2_MarcanoAbreuJesusAntonio/src/Dao/ClassUsuarioImp.java
@@ -0,0 +1,14 @@
+package Dao;
+
+import Interfaces.Iusuario;
+import model.TblUsuariocl2;
+
+public class ClassUsuarioImp implements Iusuario{
+
+	@Override
+	public TblUsuariocl2 Login(String username, String password) {
+		
+		return null;
+	}	// fin del metodo login
+
+}
---
+++ LPII_CL2_MarcanoAbreuJesusAntonio/src/Interfaces/Iusuario.java
@@ -0,0 +1,7 @@
+package Interfaces;
+
+import model.TblUsuariocl2;
+
+public interface Iusuario {
+	public TblUsuariocl2 Login(String username, String password);
+}
